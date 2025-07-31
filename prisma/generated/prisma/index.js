d488d103ba2acb411245273ab00720e9a784500a
Juan Antonio <juananthonio23@gmail.com>
2025-07-31 16:53:16 +0700

    fix: update rootEnvPath in generated Prisma files and remove Windows query engine binary

--- prisma/generated/prisma/index.js
+++ prisma/generated/prisma/index.js
@@ -241,7 +241,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
